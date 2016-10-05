18daf2864762f3b85af0fe48f25baf0f1e688cbe
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2016-10-05 19:33:43 +0200

    Extract functions

--- src/main/java/ndr/brt/mapper/Mapper.java
+++ src/main/java/ndr/brt/mapper/Mapper.java
@@ -1,11 +1,10 @@
 package ndr.brt.mapper;
 
-import com.sun.xml.internal.ws.util.StringUtils;
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.Arrays.stream;
 
 public class Mapper<S, D> {
     private final Class<S> sourceClass;
@@ -20,32 +19,66 @@ public class Mapper<S, D> {
     public D map(S source) {
         try {
             D destination = destinationClass.newInstance();
-            Arrays.stream(sourceClass.getMethods())
-                .filter(c -> c.getName().startsWith("get"))
-                .filter(c -> !"getClass".equals(c.getName()))
-                .forEach(c -> {
-                    String methodName = c.getName();
-                    String name = methodName.substring(3);
-                    String decapitalizedName = StringUtils.decapitalize(name);
-                    if (configuration.containsKey(decapitalizedName)) {
-                        name = StringUtils.capitalize(configuration.get(decapitalizedName));
-                    }
+
+            stream(sourceClass.getDeclaredFields())
+                .map(c -> extractNameAndValue(c, source))
+                .map(this::applyConfiguration)
+                .forEach(nv -> {
                     try {
-                        Method set = destinationClass.getMethod("set" + name, String.class);
-                        Object defaultValue = c.invoke(source);
-                        set.invoke(destination, defaultValue);
+                        Field destinationField = destinationClass.getDeclaredField(nv.getName());
+                        destinationField.setAccessible(true);
+                        destinationField.set(destination, nv.getValue());
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
                 });
+
             return destination;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        catch (Exception e) {
+    }
+
+    private NameAndValue applyConfiguration(NameAndValue nv) {
+        String sourceName = nv.getName();
+
+        if (configuration.containsKey(sourceName)) {
+            return new NameAndValue(configuration.get(sourceName), nv.getValue());
+        }
+        else {
+            return nv;
+        }
+
+    }
+
+    private NameAndValue extractNameAndValue(Field c, S source) {
+        c.setAccessible(true);
+        try {
+            return new NameAndValue(c.getName(), c.get(source));
+        } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void configure(String source, String destination) {
         configuration.put(source, destination);
+    }
+
+    private class NameAndValue {
+        private final String name;
+        private final Object value;
+
+        public NameAndValue(String name, Object value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Object getValue() {
+            return value;
+        }
     }
 }
